e84fac6aa986b017b5462597eea0e1ac18e8707d
Ustin Vaskin <ustin.vaskin2011@gmail.com>
2020-01-07 10:53:11 +0000

    routes

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# HttpResponse is from django 
 
 
+# function to return in a route 
 def home(request):
     return HttpResponse('<h1>Blog Home</h1>')
 
+# function to return in a route 
 
 def about(request):
     return HttpResponse('<h1>Blog About</h1>')
