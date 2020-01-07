9c495911a9448b349d6a155de0db5cb3711cca61
Ustin Vaskin <ustin.vaskin2011@gmail.com>
2020-01-07 11:24:55 +0000

    templates

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# HttpResponse is from django 
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
 
 
-# function to return in a route 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
-# function to return in a route 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
